e5e8a5436d9f359b7c2a406ffebf712b737d5214
bcode <47030277+bcode2@users.noreply.github.com>
2022-04-24 11:15:34 +0000

    add reference

--- lowercase-all-filenames.sh
+++ lowercase-all-filenames.sh
@@ -1,3 +1,4 @@
+# reference: https://stackoverflow.com/questions/152514/how-do-i-rename-all-folders-and-files-to-lowercase-on-linux
 find .  -type f -print0 | xargs -0n 1 bash -c \
 's=$(dirname "$0")/$(basename "$0");
 d=$(dirname "$0")/$(basename "$0"|tr "[A-Z]" "[a-z]"); mv -f "$s" "$d"'
